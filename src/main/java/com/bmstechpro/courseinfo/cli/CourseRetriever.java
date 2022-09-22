f42bfc4372036ee43eb4e5c46d70eb105f03e8ef
konstantinstaykov <k.staykov@gmail.com>
2022-09-21 22:35:15 -0400

    initial setup of our application

--- src/main/java/com/bmstechpro/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/bmstechpro/courseinfo/cli/CourseRetriever.java
@@ -5,4 +5,23 @@ package com.bmstechpro.courseinfo.cli;
  */
 
 public class CourseRetriever {
+    public static void main(String[] args) {
+        System.out.println("CourseRetriever Started!");
+        if (args.length == 0) {
+            System.out.println("Please provide course author name as first argument");
+            return;
+        }
+        try {
+            retrieveCourses(args[0]);
+
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void retrieveCourses(String authorId) {
+        System.out.println("Retrieving courses for author " + authorId);
+    }
 }
