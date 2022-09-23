fec9070ceb82f66e5277334c7e2c07700f5a8123
konstantinstaykov <k.staykov@gmail.com>
2022-09-22 23:23:31 -0400

    added logging to our application.

--- src/main/java/com/bmstechpro/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/bmstechpro/courseinfo/cli/CourseRetriever.java
@@ -4,24 +4,28 @@ package com.bmstechpro.courseinfo.cli;
  * @author Konstantin Staykov
  */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        System.out.println("CourseRetriever Started!");
+        LOG.info("CourseRetriever Starting");
         if (args.length == 0) {
-            System.out.println("Please provide course author name as first argument");
+            LOG.warn("Please provide course author name as first argument");
             return;
         }
         try {
             retrieveCourses(args[0]);
 
         } catch (Exception e) {
-            System.out.println("Unexpected error");
-            e.printStackTrace();
+            LOG.error("Unexpected error", e);
         }
 
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for author " + authorId);
+        LOG.info("Retrieving courses for author '{}'", authorId);
     }
 }
