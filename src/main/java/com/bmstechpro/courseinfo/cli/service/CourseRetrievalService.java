3455207c17d7027cbd12fdcc30ad37c0554da386
konstantinstaykov <k.staykov@gmail.com>
2022-09-25 22:20:46 -0400

    added CourseRetrievalService

--- src/main/java/com/bmstechpro/courseinfo/cli/service/CourseRetrievalService.java
+++ src/main/java/com/bmstechpro/courseinfo/cli/service/CourseRetrievalService.java
@@ -3,9 +3,29 @@ package com.bmstechpro.courseinfo.cli.service;
  * @created 09/22/2022
  */
 
-public class CourseRetrievalService {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-    public String getCoursesFor(String authorId){
-        return "";
+public class CourseRetrievalService {
+    private static final String PS_URU = "https://app.pluralsight.com/profile/data/author/%s/all-content";
+
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
+    public String getCoursesFor(String authorId) {
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create(PS_URU.formatted(authorId)))
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Could not call Pluralsight API", e);
+        }
+
     }
 }
