c4e68dbdc5fed8cc159d1590b2bd823379f1caf1
konstantinstaykov <k.staykov@gmail.com>
2022-09-26 22:57:53 -0400

    Introducing modules and structuring our program into modules so they can be independently build.

--- course-info-cli/src/test/java/com/bmstechpro/courseinfo/cli/service/PluralsightCourseTest.java
+++ course-info-cli/src/test/java/com/bmstechpro/courseinfo/cli/service/PluralsightCourseTest.java
@@ -1,6 +1,5 @@
 package com.bmstechpro.courseinfo.cli.service;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
