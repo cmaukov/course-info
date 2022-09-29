7be79cd85153ca330a3ed2c9e3637cf045148546
konstantinstaykov <k.staykov@gmail.com>
2022-09-28 23:01:25 -0400

    Unifying Application Logging

--- course-info-server/src/main/java/com/bmstechpro/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/bmstechpro/courseinfo/server/CourseServer.java
@@ -9,10 +9,16 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.net.URI;
+import java.util.logging.LogManager;
 
 public class CourseServer {
+    static{
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
     private static final String BASE_URI = "http://localhost:8080/";
 
