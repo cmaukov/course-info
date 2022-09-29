4bb05c28486aa9527ecfbc8c24b341150f0ce70e
konstantinstaykov <k.staykov@gmail.com>
2022-09-28 23:15:32 -0400

    Intro to external configuration

--- course-info-server/src/main/java/com/bmstechpro/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/bmstechpro/courseinfo/server/CourseServer.java
@@ -11,22 +11,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
 import java.util.logging.LogManager;
 
 public class CourseServer {
-    static{
+    static {
         LogManager.getLogManager().reset();
         SLF4JBridgeHandler.install();
     }
+
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
     private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args) {
-        LOG.info("Starting HTTP Server");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFile = loadDatabaseFileName();
+        LOG.info("Starting HTTP Server with database {}", databaseFile);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFile);
         ResourceConfig resourceConfig = new ResourceConfig().register(new CourseResource(courseRepository));
 
-        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),resourceConfig);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+    }
+
+    private static String loadDatabaseFileName() {
+        try (InputStream propertiesStream = CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("course-info.database");
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load database filename");
+        }
+
     }
 }
